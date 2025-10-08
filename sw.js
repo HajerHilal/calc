2257d88775f852efd3fe1b64df31ef8e02c1dad7
HajerHilal <hajarjob2000@gmail.com>
2025-10-08 13:06:10 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,21 @@
 const cacheName = 'counter-app-cache-v1';
 const filesToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon.png'
+  '/CounterApp/',
+  '/CounterApp/index.html',
+  '/CounterApp/manifest.json',
+  '/CounterApp/icon.png'
 ];
 
-// تثبيت الكاش
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event=>{
   console.log('Service Worker: Installed');
   event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
-    })
+    caches.open(cacheName).then(cache=>cache.addAll(filesToCache))
   );
   self.skipWaiting();
 });
 
-// استدعاء الملفات من الكاش
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event=>{
   event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request);
-    })
+    caches.match(event.request).then(resp=>resp||fetch(event.request))
   );
 });
-
