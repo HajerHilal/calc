8c90e72ca442dfb935e6a116a89ae4c43991689b
HajerHilal <hajarjob2000@gmail.com>
2025-10-08 12:53:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,28 @@
-self.addEventListener('install', e => {
+const cacheName = 'counter-app-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+// تثبيت الكاش
+self.addEventListener('install', (event) => {
   console.log('Service Worker: Installed');
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+// استدعاء الملفات من الكاش
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
+  );
 });
+
